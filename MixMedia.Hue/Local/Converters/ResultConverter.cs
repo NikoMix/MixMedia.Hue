8fe45a159380b6a9d9ee038331a2ccb039ad3b86
Niko Mix <mix.niko@outlook.com>
2017-10-20 23:18:34 +0200

    Pluralisation of Managers, prepare Messaging System

---
+++ MixMedia.Hue/Local/Converters/ResultConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MixMedia.Hue.Local.Converters
+{
+    public class ResultConverter
+    {
+    }
+}
---
+++ MixMedia.Hue/Local/Models/Messaging/ResultCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MixMedia.Hue.Local.Converters;
+using Newtonsoft.Json;
+
+namespace MixMedia.Hue.Local.Models.Messaging
+{
+    [JsonConverter(typeof(ResultConverter))]
+    public class ResultCollection : List<IResult>
+    {
+    }
+}
